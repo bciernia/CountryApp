d06d47fc6435cd6aecd03961dde6d8f4688c0513
Bartek <cierniabartek0802@gmail.com>
2022-11-19 17:15:49 +0100

    Refactor card module

--- modules/card/card.js
+++ modules/card/card.js
@@ -1,28 +1,46 @@
 import { createCountryDetailsBtn } from '../../js/createCountryDetailsBtn.js';
 
+const createImg = (src, alt, additionalClasses) => {
+  const img = document.createElement('img');
+
+  img.src = src;
+  img.alt = alt;
+  img.classList.add(...additionalClasses);
+
+  return img;
+};
+
+const createH3 = (text) => {
+  const h3 = document.createElement('h3');
+  h3.innerText = text;
+
+  return h3;
+};
+
+const createParagraph = (text) => {
+  const p = document.createElement('p');
+  p.innerText = text;
+
+  return p;
+};
+
+const createDiv = (additionalClasses) => {
+  const div = document.createElement('div');
+  div.classList.add(...additionalClasses);
+
+  return div;
+};
+
 export const createCard = (country, onDetailsBtnClick) => {
   const {
     name, capital, region, flag,
   } = country;
-  const countryFlag = document.createElement('img');
-  const countryNameH3 = document.createElement('h3');
-  const countryRegionH3 = document.createElement('p');
-  const countryCapitalH3 = document.createElement('p');
-  const card = document.createElement('div');
 
-  countryFlag.src = flag;
-  countryFlag.alt = `${name} flag`;
-  countryFlag.classList.add('country-flag-img');
-  countryNameH3.innerText = name;
-
-  countryRegionH3.innerText = region;
-
-  if (capital !== undefined) {
-    countryCapitalH3.innerText = capital;
-  } else {
-    countryCapitalH3.innerText = 'None';
-  }
-
+  const card = createDiv(['card-info']);
+  const countryFlag = createImg(flag, `${name} flag`, ['card-img']);
+  const countryNameH3 = createH3(name);
+  const countryCapitalH3 = createParagraph(capital || 'None');
+  const countryRegionH3 = createParagraph(region);
   const btnCountryDetails = createCountryDetailsBtn(onDetailsBtnClick, ['btn', 'btn-country-info']);
 
   card.appendChild(countryFlag);
@@ -30,7 +48,6 @@ export const createCard = (country, onDetailsBtnClick) => {
   card.appendChild(countryCapitalH3);
   card.appendChild(countryRegionH3);
   card.appendChild(btnCountryDetails);
-  card.classList.add('country-info');
 
   return card;
 };
